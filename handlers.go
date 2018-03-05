b8f03adc8e1dc7479406da880bdee3aeb3ed56bf
Paulo Soares <pauloxti@gmail.com>
2018-03-05 21:01:59 +0100

    inserta usuario desde api

--- handlers.go
+++ handlers.go
@@ -82,18 +82,41 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func signupUser(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	fmt.Println("HOLA")
 
 	var user User
-	_ = json.NewDecoder(r.Body).Decode(&user)
-	user.LastName = vars["LastName"]
-	user.FirstName = vars["FirstName"]
-	user.IdentificationDocument = vars["IdentificationDocument"]
-	user.Email = vars["Email"]
-	user.Passwd = vars["Passwd"]
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		fmt.Println(err)
+	}
+
+	// Open up our database connection.
+	//db, err := sql.Open("mysql", "user:password@tcp(hostIP:port)/database")
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/auxilium_db")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		log.Print(err.Error())
+	}
+
+	// Execute the query
+	results, err := db.Query("INSERT INTO users VALUES (7, '"+user.LastName+"', '"+user.FirstName+"', '"+user.IdentificationDocument+"', '"+user.Email+"', '"+user.Passwd+"')")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var user User
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&user.ID, &user.LastName, &user.FirstName, &user.IdentificationDocument, &user.Email, &user.Passwd)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		json.NewEncoder(w).Encode(user)
+		fmt.Fprintf(w, "User inserted")
+	}
 
 
-	json.NewEncoder(w).Encode(user)
+	// defer the close till after the main function has finished
+	// executing
+	defer db.Close()
 }
